7a103a314068fec5ec9ee846c49641c35ac6fef7
Herobrine <hugoattal@hotmail.fr>
2016-12-17 22:31:36 +0100

    Updated register page

--- pages/home/content.php
+++ pages/home/content.php
@@ -3,7 +3,7 @@
 		<span class="title">Prêt à <b>rejoindre</b> la première league <b>esport</b> étudiante ?</span><br />
 		<div id="subscription">
 			<img src="./style/img/arrow_down.png" alt="down" /><br />
-			<span class="title"><i class="fa fa-chevron-right" aria-hidden="true"></i> <a href="#">S'inscrire</a> <i class="fa fa-chevron-left" aria-hidden="true"></i></span><br />
+			<span class="title"><i class="fa fa-chevron-right" aria-hidden="true"></i> <a href="index.php?page=register">S'inscrire</a> <i class="fa fa-chevron-left" aria-hidden="true"></i></span><br />
 			<img src="./style/img/arrow_up.png" alt="down" />
 		</div>
 		<span class="title">Venez <b>défendre</b> les couleurs de votre école jusqu'au <b>podium</b> !<br />
@@ -11,16 +11,16 @@
 	<table id="games">
 		<tr>
 			<td style="background-image: url('./style/img/ban/ban_csgo.png');">
-				<span class="name"><i class="fa fa-angle-right" aria-hidden="true"></i> Counter Strike</span>
+				<span class="name"><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i> Counter Strike</a></span>
 			</td>
 			<td style="background-image: url('./style/img/ban/ban_ow.png" ');">
-				<span class="name"><i class="fa fa-angle-right" aria-hidden="true"></i> Overwatch</span>
+				<span class="name"><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i> Overwatch</a></span>
 			</td>
 			<td style="background-image: url('./style/img/ban/ban_hs.png" ');">
-				<span class="name"><i class="fa fa-angle-right" aria-hidden="true"></i> Hearthstone</span>
+				<span class="name"><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i> Hearthstone</a></span>
 			</td>
 			<td style="background-image: url('./style/img/ban/ban_lol.png"');">
-				<span class="name"><i class="fa fa-angle-right" aria-hidden="true"></i> League of Legends</span>
+				<span class="name"><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i> League of Legends</a></span>
 			</td>
 		</tr>
 	</table>
