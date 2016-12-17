ea3dce2c6d4cb4560a173554250c31a15426645a
Herobrine <hugoattal@hotmail.fr>
2016-12-17 22:45:15 +0100

    Minor updates

--- pages/register/content.php
+++ pages/register/content.php
@@ -1,27 +1,29 @@
 <div id="content">
-	<br />
-	<h1><i class="fa fa-angle-right" aria-hidden="true"></i> Inscription</h1>
-	<p>Comme l'ont dit de nombreux intellectuels avant moi :</p>
-	<div class="quote">
-		<span class="qcontent">
-			<i>&ldquo;</i>Everybody P90 ! Rush B cyka blyat !<br />Don't stop, don't stop...<i>&rdquo;</i>
-		</span>
-		<span class="qauthor">
-			- Un joueur de la SGL 2016
-		</span>
+	<div class="container">
+		<h1><i class="fa fa-angle-right" aria-hidden="true"></i> Inscription</h1>
+		<p>Comme l'ont dit de nombreux intellectuels avant moi :</p>
+		<div class="quote">
+			<span class="qcontent">
+				<i>&ldquo;</i>Everybody P90 ! Rush B cyka blyat !<br />Don't stop, don't stop...<i>&rdquo;</i>
+			</span>
+			<span class="qauthor">
+				- Un joueur de la SGL 2016
+			</span>
+		</div>
+		<p>Ce qui se traduit approximativement par "seuls les lâches abandonnent", et comme vous, vous n'êtes pas un lâche (vous êtes quelqu'un de bien, ça se voit), vous allez continuer l'inscription JUSQ'-AU-BOUT ! ('fin bon, ça prends 30 secondes max, hein...)</p>
+		<br />
+		<div class="form">
+			<table>
+				<tr><td><h3>Pseudo :</h3></td><td><input type="text" name="pseudo" /></td></tr>
+				<tr><td><h3>Mail :</h3></td><td><input type="text" name="mail" /></td></tr>
+				<tr><td><h3>Ecole :</h3></td><td><input type="text" name="school" /></td></tr>
+			</table>
+			<br /><br /><br />
+			<div class="g-recaptcha" data-sitekey="6LdkIg8UAAAAAPzgYebRn65Lx2esFnRzOF39fMBf" data-theme="dark"></div>
+			<br /><br />
+			<div class="smallquote">Je ne suis pas un robot ! Mes faux pas me collent à la peau... Je ne suis pas un robot ! Faut pas croire ce que disent les journaux...<br />Je ne suis pas un robot, un roboooooot ! (Bon ok, je sors =>[])</div>
+			<br /><br /><br />
+			<button type="submit" form="register" value="Submit">S'inscrire</button>
+		</div>
 	</div>
-	<p>Ce qui se traduit approximativement par "seuls les lâches abandonnent", et comme vous, vous n'êtes pas un lâche (vous êtes quelqu'un de bien, ça se voit), vous allez continuer l'inscription JUSQ'-AU-BOUT ! ('fin bon, ça prends 30 secondes max, hein...)</p>
-	<br />
-	<div class="form">
-		<table>
-			<tr><td><h3>Pseudo :</h3></td><td><input type="text" name="pseudo" /></td></tr>
-			<tr><td><h3>Mail :</h3></td><td><input type="text" name="mail" /></td></tr>
-			<tr><td><h3>Ecole :</h3></td><td><input type="text" name="school" /></td></tr>
-		</table>
-		<br /><br /><br />
-		<div class="g-recaptcha" data-sitekey="6LdkIg8UAAAAAPzgYebRn65Lx2esFnRzOF39fMBf" data-theme="dark"></div>
-		<br /><br /><br />
-		<button type="submit" form="register" value="Submit">S'inscrire</button>
-	</div>
-	<br />
 </div>
