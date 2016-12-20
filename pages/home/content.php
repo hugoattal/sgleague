124c0560f2a204e862b2b530ec1025e417a2d828
Herobrine <hugoattal@hotmail.fr>
2016-12-20 15:07:47 +0100

    Added games page

--- pages/home/content.php
+++ pages/home/content.php
@@ -3,24 +3,39 @@
 		<span class="title">Prêt à <b>rejoindre</b> la première league <b>esport</b> étudiante ?</span><br />
 		<div id="subscription">
 			<img src="./style/img/arrow_down.png" alt="down" /><br />
-			<span class="title"><i class="fa fa-chevron-right" aria-hidden="true"></i> <a href="index.php?page=register">S'inscrire</a> <i class="fa fa-chevron-left" aria-hidden="true"></i></span><br />
+			<span class="title"><i class="fa fa-chevron-right" aria-hidden="true"></i>
+<?php
+if (isset($_SESSION["sgl_id"]))
+{
+?>
+			<a href="index.php?page=games">Choisir un jeu</a>
+<?php
+}
+else
+{
+?>
+			<a href="index.php?page=register">S'inscrire</a>
+<?php
+}
+?>
+			<i class="fa fa-chevron-left" aria-hidden="true"></i></span><br />
 			<img src="./style/img/arrow_up.png" alt="down" />
 		</div>
 		<span class="title">Venez <b>défendre</b> les couleurs de votre école jusqu'au <b>podium</b> !<br />
 	</div>
 	<table id="games">
 		<tr>
-			<td style="background-image: url('./style/img/ban/ban_csgo.png');">
-				<span class="name"><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i> Counter Strike</a></span>
-			</td>
 			<td style="background-image: url('./style/img/ban/ban_ow.png" ');">
 				<span class="name"><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i> Overwatch</a></span>
 			</td>
-			<td style="background-image: url('./style/img/ban/ban_hs.png" ');">
-				<span class="name"><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i> Hearthstone</a></span>
-			</td>
 			<td style="background-image: url('./style/img/ban/ban_lol.png"');">
 				<span class="name"><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i> League of Legends</a></span>
+			</td>
+			<td style="background-image: url('./style/img/ban/ban_csgo.png');">
+				<span class="name"><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i> Counter Strike</a></span>
+			</td>
+			<td style="background-image: url('./style/img/ban/ban_hs.png" ');">
+				<span class="name"><a href="#"><i class="fa fa-angle-right" aria-hidden="true"></i> Hearthstone</a></span>
 			</td>
 		</tr>
 	</table>
