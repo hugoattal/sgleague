18f76bde0d479ab80741cbbfdf85391846cbc5cd
Herobrine <hugoattal@hotmail.fr>
2016-12-20 17:21:58 +0100

    Updated games page

--- pages/games/content.php
+++ pages/games/content.php
@@ -1,5 +1,7 @@
 <?php
 
+global $csrf_check;
+
 include_once("./class/Database.class.php");
 
 $database = new Database();
@@ -27,17 +29,24 @@ $games_logo = array(
 	"csgo.png",
 	"hs.png");
 
-if(isset($_GET["game"]))
+if (isset($_GET["game"]) AND $csrf_check)
 {
 	$form_game = intval($_GET["game"]);
 	if (in_array($form_game, $games))
 	{
-		$temp = $database->req('SELECT COUNT(*) as exist FROM sgl_teams WHERE user="'.$_SESSION["sgl_id"].'" AND game="'.$form_game.'"');
-		$data = $temp->fetch();
-
-		if ($data["exist"] == 0)
+		if (isset($_GET["del"]))
 		{
-			$database->req('INSERT INTO sgl_teams (user, game, lead, type, register) VALUES("'.$_SESSION["sgl_id"].'", "'.$form_game.'", "'.$_SESSION["sgl_id"].'", 1, '.time().')');
+			$database->req('DELETE FROM sgl_teams WHERE user="'.$_SESSION["sgl_id"].'" AND game="'.$form_game.'"');
+		}
+		else
+		{
+			$temp = $database->req('SELECT COUNT(*) as exist FROM sgl_teams WHERE user="'.$_SESSION["sgl_id"].'" AND game="'.$form_game.'"');
+			$data = $temp->fetch();
+
+			if ($data["exist"] == 0)
+			{
+				$database->req('INSERT INTO sgl_teams (user, game, lead, type, register) VALUES("'.$_SESSION["sgl_id"].'", "'.$form_game.'", "'.$_SESSION["sgl_id"].'", 1, '.time().')');
+			}
 		}
 	}
 }
@@ -45,7 +54,7 @@ if(isset($_GET["game"]))
 // TODO automated generation
 $games_in = array (1 => false, 2 => false, 3 => false, 4 => false);
 
-$temp = $database->req('SELECT game FROM sgl_teams WHERE lead="'.$_SESSION["sgl_id"].'"');
+$temp = $database->req('SELECT game FROM sgl_teams WHERE user="'.$_SESSION["sgl_id"].'"');
 
 while($data = $temp->fetch())
 {
@@ -70,20 +79,48 @@ while($data = $temp->fetch())
 for ($i=0; $i<count($games); $i++)
 {
 
-	echo '<p><table class="line_table"><tr><td><hr class="line" /></td><td><img src="./style/img/games/'.$games_logo[$i].'" alt="'.$games_name[$i].'" /></td><td><hr class="line" /></td></tr></table></p><br />';
+	echo '<p><table class="line_table"><tr><td><hr class="line" /></td><td><img src="./style/img/games/'.$games_logo[$i].'" alt="'.$games_name[$i].'" /></td><td><hr class="line" /></td></tr></table></p>';
 	
 // TODO check for leader invitation to accept / decline
 
 	if ($games_in[$games[$i]])
 	{
-		echo '<p>Vous êtes inscrit à ce tournoi !</p>';
+		echo '<p style="text-align: center;" class="smallquote">Vous êtes inscrit à ce tournoi ! Plus qu\'à hard train jusqu\'à début Février...</p><br />';
+
+		$temp = $database->req('SELECT sgl_users.login, sgl_teams.type
+			FROM sgl_users, sgl_teams LEFT JOIN sgl_teams AS my_team ON sgl_teams.lead = my_team.lead AND sgl_teams.game = my_team.game
+			WHERE my_team.user="'.$_SESSION["sgl_id"].'" AND my_team.game="'.$games[$i].'" AND sgl_teams.user = sgl_users.id');
+
+		$type = array("Aucun", "Leader", "Joueur", "Remplaçant");
+
+		$nplayer = 0;
+
+// TODO check stand-by players
+
+		echo '<p style="text-align: center">';
+
+		while($data = $temp->fetch())
+		{
+			$nplayer++;
+			echo '<span class="playercard"><span class="playername">'.htmlspecialchars($data["login"]).'</span><span class="playertype">('.$type[$data["type"]].')</span></span>';
+		}
+
+		if ($nplayer < $games_team[$i])
+		{
+			echo '<span class="buttoncard">Ajouter un joueur</span>';
+		}
+
+		echo '</p><br /><br /><br />';
+
+// TODO confirm delete participation
+
 		echo '<p style="text-align: center;"><a href="index.php?page=games&amp;game='.$games[$i].'&amp;del=1" class="button">Se désinscire du tournoi</a></p>';
 	}
 	else
 	{
-		if ($games["team"] > 1)
+		if ($games_team[$i] > 1)
 		{
-			echo '<p>Si vous souhaitez rejoindre une équipe, votre chef d\'équipe doit d\'abord vous inviter.</p>';
+			echo '<p style="text-align: center;" class="smallquote">Si vous souhaitez rejoindre une équipe, votre chef d\'équipe doit d\'abord vous inviter.</p><br /<br />';
 		}
 		echo '<p style="text-align: center;"><a href="index.php?page=games&amp;game='.$games[$i].'" class="button">S\'inscire au tournoi</a></p>';
 	}
